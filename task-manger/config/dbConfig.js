d20031bdd1a3f2fccb24d0d59863efbd137a71fa
ram <bvn.ramkumar@gmail.com>
2025-02-15 00:49:48 +0530

    Established MongoDB connection

--- task-manger/config/dbConfig.js
+++ task-manger/config/dbConfig.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const connectDB = async() =>{
     try{
-        await mongoose.connect("mongodb://localhost:27017/guviTaskManager");
+        await mongoose.connect("mongodb+srv://guvi:guvi123@cluster0.g2kyq.mongodb.net/");
         console.log("Connected to MongoDB");
     }   catch(error){
         console.log("Error connecting to MongoDB", error);
